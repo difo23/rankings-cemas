4606ea9b4e4eba7ba187595ee20ac6d614027841
difo23 <diffozanzan@gmail.com>
2021-02-20 17:00:25 -0400

    Crear hooks useForm

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
       <div className="row mt-4">
 
         <div className="col">
+          <h1>Records:</h1>
+          <hr />
+        </div>
+
+      </div>
+
+      <div className="row mt-4">
+
+        <div className="col">
           <SearchBar />
         </div>
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,11 +4,11 @@ export default function SearchBar() {
     return (
         <div>
             <form>
-                <div class="form-row">
-                    <div class="col">
+                <div class="form-row ml-5">
+                    <div class="col-5">
                         <input type="text" className="form-control" placeholder="Curso" />
                     </div>
-                    <div class="col">
+                    <div class="col-5">
                         <input type="text" className="form-control" placeholder="Periodo" />
                     </div>
                     <div class="col-2">
