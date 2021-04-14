0bcb5d308c631bc8869cd955cb538227e87d3dc6
Adonys Santos <adonysmundotech@gmail.com>
2021-04-14 11:02:42 -0400

    Declarando variables

--- src/components/helpers/get-idx-academico.js
+++ src/components/helpers/get-idx-academico.js
@@ -1,29 +1,29 @@
 const getIdxAcademico = (asignaturas) => {
   if (asignaturas) {
-    let promedio_asignaturas = 0;
     let sum_asignatura = 0;
+    let promedio_asignatura = 0;
+    let sum_promedio_asignaturas = 0;
+    let promedio_academico = 0;
 
     //Calculamos indx con asignaturas
     asignaturas.forEach((element) => {
-      for (let i = 0; i < asignatura.length; i++) {
+      for (let i = 0; i < asignaturas.length; i++) {
         sum_asignatura =
           parseInt(asignaturas[i].ago_sept_oct) +
           parseInt(asignaturas[i].nov_dic_ene) +
           parseInt(asignaturas[i].feb_mar) +
           parseInt(asignaturas[i].abr_may_jun);
-        // "ago_sept_oct"
-        // "nov_dic_ene"
-        // "feb_mar"
-        // "abr_may_jun"
 
-        // es la suma de los 4 meses dividido entre 4
-        promedio_asignaturas = sum_asignatura / 4;
+        // Suma de todos los meses divididos entre la cantidad de meses.
+        promedio_asignatura = sum_asignatura / 4;
 
         console.log(typeof object, typeof data);
         console.log("getIdxAcademico", asignaturas);
-        console.log("promedio de las asignaturas", promedio_asignaturas);
+        console.log("promedio de las asignaturas", promedio_asignatura);
       }
+      promedio_academico += promedio_asignatura;
     });
+    console.log("promedio academico:", promedio_academico);
     return promedio_academico;
   } else {
     // Return 0 si asignaturas es null o []
