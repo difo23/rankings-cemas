a7ac1b6e23fff54b3b56292eaeb254c779e7b080
Estheixys Peña <pehinojosa72@gmail.com>
2021-04-15 16:15:02 -0400

    Calcular el promedio tecnico de 6to

--- src/components/helpers/get-idx-tecnico.js
+++ src/components/helpers/get-idx-tecnico.js
@@ -3,19 +3,29 @@ const getIdxTecnico = (modulos, curso) => {
     modulos_length = 0,
     idx_tecnico = 0;
 
-    switch (curso[0]) {
-      case "4":
-      case "5":
-        // Código de Vane...
-        break;
-      case "6":
-        // Código de Tesy...
-        break;
-      default:
-        // Si retorna null es porque no exixten materias técnicas.
-        return null;
-    }
+  switch (curso[0]) {
+    case "4":
+    case "5":
+      // Código de Vane...
+      break;
+    case "6":
+      // Recorrer los modulos
+      modulos.forEach((asignatura, index) => {
+        if (asignatura.codigo_asignatura == "MF0000_FCT") {
+          let fct_transformado = (asignatura.acumulado * 100) / asignatura.total;
+          let fct_acumulado = fct_transformado;
+          sum_acumulado_modulos += fct_acumulado;
+        } else sum_acumulado_modulos += asignatura.acumulado;
+        modulos_length = index;
+      });
+
+      // Calcular el indice tecnico:
+      idx_tecnico = sum_acumulado_modulos / (modulos_length + 1);
+      return idx_tecnico;
+    default:
+      // Si retorna null es porque no exixten materias técnicas.
+      return null;
+  }
 };
 
- export default  getIdxTecnico;
- 
+export default getIdxTecnico;
