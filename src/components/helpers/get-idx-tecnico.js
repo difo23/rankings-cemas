79343e3f54f778321304cbbc5d245bab52711012
Estheixys Peña <pehinojosa72@gmail.com>
2021-04-15 15:50:25 -0400

    Crear Switch

--- src/components/helpers/get-idx-tecnico.js
+++ src/components/helpers/get-idx-tecnico.js
@@ -3,7 +3,18 @@ const getIdxTecnico = (modulos, curso) => {
     modulos_length = 0,
     idx_tecnico = 0;
 
-  return 0;
+    switch (curso[0]) {
+      case "4":
+      case "5":
+        // Código de Vane...
+        break;
+      case "6":
+        // Código de Tesy...
+        break;
+      default:
+        // Si retorna null es porque no exixten materias técnicas.
+        return null;
+    }
 };
 
  export default  getIdxTecnico;
