0292b83ae5bcde6e2cf8c2ac3f06edd8ffaa1dea
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-04-15 16:34:40 -0400

    Calcular promedio técnico de 4to y 5to

--- src/components/helpers/get-idx-tecnico.js
+++ src/components/helpers/get-idx-tecnico.js
@@ -3,19 +3,25 @@ const getIdxTecnico = (modulos, curso) => {
     modulos_length = 0,
     idx_tecnico = 0;
 
-    switch (curso[0]) {
-      case "4":
-      case "5":
-        // Código de Vane...
-        break;
-      case "6":
-        // Código de Tesy...
-        break;
-      default:
-        // Si retorna null es porque no exixten materias técnicas.
-        return null;
-    }
+  switch (curso[0]) {
+    case "4":
+    case "5":
+      // Recorrer los modulos
+      modulos.forEach((asignatura, index) => {
+        sum_acumulado_modulos += asignatura.acumulado;
+        modulos_length = index;
+      });
+
+      // Obtener el promedio de los modulos
+      idx_tecnico = sum_acumulado_modulos / (modulos_length + 1);
+      return idx_tecnico;
+    case "6":
+      // Código de Tesy...
+      break;
+    default:
+      // Si retorna null es porque no exixten materias técnicas.
+      return null;
+  }
 };
 
- export default  getIdxTecnico;
- 
+export default getIdxTecnico;
