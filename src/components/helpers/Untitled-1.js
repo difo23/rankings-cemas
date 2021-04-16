6d7c68cd68eda41819ff4d767fd06127b633ce44
Adonys Santos <adonysmundotech@gmail.com>
2021-04-15 23:29:24 -0400

    Enviando arreglo completo a la funcion getIdexAcademico

---
+++ src/components/helpers/Untitled-1.js
@@ -0,0 +1,18 @@
+// Calcular idx academico
+
+/**
+   * Ejemplo  de los elementos dentro de asignaturas:
+   * {
+        "codigo_asignatura": "EDAR004",
+        "ago_sept_oct": "83",
+        "nov_dic_ene": "0",
+        "feb_mar": "0",
+        "abr_may_jun": "0",
+        "cf": 21
+      }
+   */
+
+/**
+   * Formula:
+    idx_academico = (sum_cf_asignaturas)/ (asignaturas_length)
+   */
