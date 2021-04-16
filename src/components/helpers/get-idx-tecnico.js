e7241c464aba90636febc6bd84460fc61d5de255
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2021-04-16 13:45:27 -0400

    Revert "Calculamos el promedio técnico "

--- src/components/helpers/get-idx-tecnico.js
+++ src/components/helpers/get-idx-tecnico.js
@@ -1,38 +1,66 @@
-const getIdxTecnico = (modulos, curso) => {
-  let sum_acumulado_modulos = 0,
-    modulos_length = 0,
-    idx_tecnico = 0;
 
-  switch (curso[0]) {
-    case "4":
-    case "5":
-      // Recorrer los modulos
-      modulos.forEach((asignatura, index) => {
-        sum_acumulado_modulos += parseFloat (asignatura.acumulado);
-        modulos_length = index;
-      });
 
-      // Obtener el promedio de los modulos
-      idx_tecnico = sum_acumulado_modulos / (modulos_length + 1);
-      return idx_tecnico;
-    case "6":
-      // Recorrer los modulos
-      modulos.forEach((asignatura, index) => {
-        if (asignatura.codigo_asignatura == "MF0000_FCT") {
-          let fct_transformado = (asignatura.acumulado * 100) / asignatura.total;
-          let fct_acumulado = fct_transformado;
-          sum_acumulado_modulos += fct_acumulado;
-        } else sum_acumulado_modulos += asignatura.acumulado;
-        modulos_length = index;
-      });
+export default function getIdxTecnico(modulos, curso) {
 
-      // Calcular el indice tecnico:
-      idx_tecnico = sum_acumulado_modulos / (modulos_length + 1);
-      return idx_tecnico;
-    default:
-      // Si retorna null es porque no exixten materias técnicas.
-      return null;
-  }
-};
+    // Calcular idx tecnico
 
-export default getIdxTecnico;
+    console.log('getIdxTecnico', modulos);
+
+    /**
+     * Ejemplo  de los elementos dentro de modulos:
+     * {
+          "codigo_asignatura": "MF3623_GDD",
+          "acumulado": 23,
+          "total": 25
+        }
+     */
+
+    /**
+     * Formula:
+     * 
+     * Para 4to y 5to: 
+        calculo en idx directamente con la formula:
+
+        idx_tecnico = (sum_acumulado_modulos)/ (modulos_length)
+     
+    * Para 6to:
+        Es necesario aplicar regla de 3 al acumulado de la asignatura
+        Formacion de Centros de trabajos (FCT), su codigo asignatura es MF0000_FCT.
+
+        Ejemplo 6A:
+        {
+          "codigo_asignatura": "MF0000_FCT",
+          "acumulado": 26,
+          "total": 30
+        },
+
+
+        fct_transformada = (fct_acumulado * 100) / (fct_total)
+
+        Luego actualizo el valor de fct_acumulado por el nuevo valor transformado.
+
+        fct_acumulado = fct_transformado
+
+        Y calculo el indice tecnico: 
+
+        idx_tecnico = (sum_acumulado_modulos)/ (modulos_length)
+
+     */
+
+
+    if (modulos) {
+
+        // Calcular el indice 
+
+
+        return 93.8842;
+
+    } else {
+
+        // Si modulos es null o [] retorna 0
+        // Esto sucede cuando no son cursos tecnicos
+        return 0;
+    }
+
+
+}
