4ece3d248899b54fba323b7f1ac7bb25ed0ebca5
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2021-04-17 23:49:50 -0400

    Revert "Entrando asignatura.acumulado en un parseFloat"

--- src/components/helpers/get-idx-academico.js
+++ src/components/helpers/get-idx-academico.js
@@ -1,36 +1,21 @@
-
-
-export default function getIdxAcademico(asignaturas) {
-
-  console.log('getIdxAcademico', asignaturas);
-
-  // Calcular idx academico
-
-  /**
-   * Ejemplo  de los elementos dentro de asignaturas:
-   * {
-        "codigo_asignatura": "EDAR004",
-        "ago_sept_oct": "83",
-        "nov_dic_ene": "0",
-        "feb_mar": "0",
-        "abr_may_jun": "0",
-        "cf": 21
-      }
-   */
-
-  /**
-   * Formula:
-    idx_academico = (sum_cf_asignaturas)/ (asignaturas_length)
-   */
+const getIdxAcademico = (asignaturas) => {
+  let sum_calificacion_final = 0,
+    asignaturas_length = 0,
+    idx_academico = 0;
 
   if (asignaturas) {
-    //Calculamos index con asignaturas
+    // Recorrer las asignaturas
+    asignaturas.forEach((asignatura, index) => {
+      sum_calificacion_final += parseFloat(asignatura.cf);
+      asignaturas_length = index;
+    });
 
-    return 93.9042;
-  } else {
-
-    // Return 0 si asignaturas es null o []
-    return 0;
+    // Obtener el promedio de las asignaturas
+    idx_academico = sum_calificacion_final / (asignaturas_length + 1);
+    return idx_academico;
   }
+  // Return 0 si asignaturas es null o []
+  else return 0;
+};
 
-}
+export default getIdxAcademico;
