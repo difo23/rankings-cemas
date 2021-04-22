a63d325f3d45d7fe51fdf5a63f7c24e87c10c7c8
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-04-22 13:13:24 -0400

    Agregando logo y título

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,11 @@ const Menu = () => {
   return (
   <header>
     <nav>
-      Menu
+    <div class="cemas">
+          <img class="logo" src="../../assets/imgs/logo.png" alt="" />
+          <h1 class="title">Ranking Cemas</h1>
+        </div>
+        {/* Boton para salir dentro de un Div */}
     </nav>
   </header>
   );
