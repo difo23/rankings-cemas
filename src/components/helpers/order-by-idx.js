c5cee09e43a23b9657f799c89ee1124d3cfd8d3c
Adonys Santos <adonysmundotech@gmail.com>
2021-05-03 17:56:59 -0400

    feat: Agrega caso para el numero del estudiante

--- src/components/helpers/order-by-idx.js
+++ src/components/helpers/order-by-idx.js
@@ -1,14 +1,22 @@
-export default function orderByIdx(ranking) {
+import filter from "./filter";
 
-    return ranking.sort(function (a, b) {
-        if (a.idx_general < b.idx_general) {
-            return 1;
-        }
-        if (a.idx_general > b.idx_general) {
-            return -1;
-        }
-        // a must be equal to b
-        return 0;
-    });
+const orderByIdx = (ranking) => {
+  return ranking.sort(function (a, b, selected = "num-lista") {
+    switch (selected) {
+      // idx_academico: 49.75
+      // idx_general: 58
+      // idx_tecnico: 66.25
+      // numero_estudiante: "4" = 4
+      case "num-lista":
+        filter(
+          parseFloat(a.numero_estudiante),
+          parseFloat(b.numero_estudiante)
+        );
+        break;
+      default:
+        return null;
+    }
+  });
+};
 
-}
+export default orderByIdx;
