cdf94401249ebf7c288691710d359b09b3a6f5e8
Rusbert8 <rusbertsanchez8@gmail.com>
2021-05-03 19:40:25 -1200

    Arregla un error provocado por el Merge

--- src/components/ItemBoletin.js
+++ src/components/ItemBoletin.js
@@ -8,8 +8,8 @@ const ItemBoletin = ({ estudiante }) => {
             <article>
                 <b className="nombre">{estudiante.nombre_estudiante}<b className="numero">{estudiante.numero_estudiante}</b></b>
                 <ul><b>
-                    <li>Asignaturas: {estudiante.asignaturas.map(asignatura => {asignatura.codigo_asignatura} )}</li>
-                    <li>Modulos: {estudiante.modulos.map(modulo => {modulo.codigo_asignatura} )}</li>
+                    <li>Asignaturas: {estudiante.asignaturas.map(asignatura => <span>{asignatura.codigo_asignatura}</span> )}</li>
+                    <li>Modulos: {estudiante.modulos.map(modulo => <span>{modulo.codigo_asignatura}</span> )}</li>
                     <li>{`Promedio Academico : ${estudiante.idx_academico}`}</li>
                     <li>{`Promedio Tecnico : ${estudiante.idx_tecnico}`}</li>
                     <li>{`Promedio General : ${estudiante.idx_general}`}</li>
