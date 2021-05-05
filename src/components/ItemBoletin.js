3f34cc588a4a928b052fc1fe431b6e6cdfbb722e
Rusbert8 <rusbertsanchez8@gmail.com>
2021-05-04 16:13:02 -1200

    Da estilos a los boletines

--- src/components/ItemBoletin.js
+++ src/components/ItemBoletin.js
@@ -7,8 +7,8 @@ const ItemBoletin = ({ estudiante }) => {
             <article>
                 <h3 className="nombre">{estudiante.nombre_estudiante} <span className="numero">{estudiante.numero_estudiante}</span></h3>
                 <div className="datos">
-                    {/* <li><span>Asignaturas :</span> {estudiante.asignaturas.map(asignatura => <span>{asignatura.codigo_asignatura} </span> )}</li>
-                    <li><span>Modulos :</span> {estudiante.modulos.map(modulo => <span>{modulo.codigo_asignatura}</span> )}</li> */}
+                    <p><span>Asignaturas :</span> {estudiante.asignaturas.map(asignatura => <span>{asignatura.codigo_asignatura} </span> )}</p>
+                    <p><span>Modulos :</span> {estudiante.modulos.map(modulo => <span>{modulo.codigo_asignatura}</span> )}</p>
                     <p><span>Promedio Acádemico :</span> {`${estudiante.idx_academico}`}</p>
                     <p><span>Promedio Técnico :</span> {`${estudiante.idx_tecnico}`}</p>
                     <p><span>Promedio General :</span> {`${estudiante.idx_general}`}</p>
