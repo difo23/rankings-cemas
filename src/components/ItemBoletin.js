ad00a096fa3651b310937fb4c103887e9bfb67fe
Rusbert8 <rusbertsanchez8@gmail.com>
2021-05-04 13:55:56 -1200

    Añade estilos a los datos de los boletines

--- src/components/ItemBoletin.js
+++ src/components/ItemBoletin.js
@@ -4,19 +4,16 @@ const ItemBoletin = ({ estudiante }) => {
     const [active] = useState('')
 
     return (
-
             <article>
-                <h3 className="nombre">{estudiante.nombre_estudiante}<span className="numero">{estudiante.numero_estudiante}</span></h3>
-                <ul>
-                    <li>Asignaturas: {estudiante.asignaturas.map(asignatura => <span>{asignatura.codigo_asignatura}</span> )}</li>
-                    <li>Modulos: {estudiante.modulos.map(modulo => <span>{modulo.codigo_asignatura}</span> )}</li>
-                    <li>{`Promedio Acádemico : ${estudiante.idx_academico}`}</li>
-                    <li>{`Promedio Técnico : ${estudiante.idx_tecnico}`}</li>
-                    <li>{`Promedio General : ${estudiante.idx_general}`}</li>
+                <h3 className="nombre">{estudiante.nombre_estudiante} <span className="numero">{estudiante.numero_estudiante}</span></h3>
+                <ul className="datos">
+                    <li><span>Asignaturas :</span> {estudiante.asignaturas.map(asignatura => <span>{asignatura.codigo_asignatura} </span> )}</li>
+                    <li><span>Modulos :</span> {estudiante.modulos.map(modulo => <span>{modulo.codigo_asignatura}</span> )}</li>
+                    <li><span>Promedio Acádemico :</span> {`${estudiante.idx_academico}`}</li>
+                    <li><span>Promedio Técnico :</span> {`${estudiante.idx_tecnico}`}</li>
+                    <li><span>Promedio General :</span> {`${estudiante.idx_general}`}</li>
                 </ul>
             </article>
-
-
     )
 }
 
