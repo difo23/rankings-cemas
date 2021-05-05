2578ec2ad12989370480554044dae594de108161
Rusbert8 <rusbertsanchez8@gmail.com>
2021-05-04 16:23:54 -1200

    Corrije una falta ortográfica en los boletines

--- src/components/ItemBoletin.js
+++ src/components/ItemBoletin.js
@@ -8,7 +8,7 @@ const ItemBoletin = ({ estudiante }) => {
                 <h3 className="nombre">{estudiante.nombre_estudiante} <span className="numero">{estudiante.numero_estudiante}</span></h3>
                 <div className="datos">
                     <p><span>Asignaturas :</span> {estudiante.asignaturas.map(asignatura => <span>{asignatura.codigo_asignatura} </span> )}</p>
-                    <p><span>Modulos :</span> {estudiante.modulos.map(modulo => <span>{modulo.codigo_asignatura}</span> )}</p>
+                    <p><span>Módulos :</span> {estudiante.modulos.map(modulo => <span>{modulo.codigo_asignatura}</span> )}</p>
                     <p><span>Promedio Acádemico :</span> {`${estudiante.idx_academico}`}</p>
                     <p><span>Promedio Técnico :</span> {`${estudiante.idx_tecnico}`}</p>
                     <p><span>Promedio General :</span> {`${estudiante.idx_general}`}</p>
